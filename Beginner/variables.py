48b43b112e6b14cfe5688a216d71fa8c6d09ead0
Kardel Rüveyda Çetin <ruveydakardelcetin@gmail.com>
2020-06-29 21:14:43 +0300

    Variables

--- Beginner/variables.py
+++ Beginner/variables.py
@@ -1,3 +1,15 @@
-a = 10 ; 
+sayi = 10  #integer
+tc="10"   #string
+isim ="Kardel" #string
+soyisim="Çetin"
+telefonNumarasi = "05352867586"
+floatSayi = 61.0
 
-print(a);
+#Yorum Satırı diyez ile gerçekleşir.
+#
+
+
+print(float(tc)+10)
+print(type(floatSayi))
+print(type(sayi))
+print(type(tc))
